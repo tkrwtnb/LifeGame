34e799ecfb1212ce9cb0fb0180bc8bad822b1a43
tkrwtnb <wtnbtkr0000@gmail.com>
2021-01-23 21:44:03 +0900

    add: grider, blinker, galaxy

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from icecream import ic
 import time 
 import os 
 import random 
-LINE_LEN = 50
+
+LINE_LEN = 30
+SLEEP_TIME = 0.25
 
 clear = lambda: os.system('clear')
 
@@ -28,12 +30,22 @@ def initialize_board(board):
     board.clear()
 
     for i in range(0, LINE_LEN):
-        line = [False] * LINE_LEN
+        line = [True] * LINE_LEN
         rand_array = [random.randrange(LINE_LEN)] * 1000
 
         for random_index in rand_array:
-            line[random_index] = True
+            line[random_index] = False
 
+        board.append(line)
+
+    return board
+
+
+def all_false_board(board):
+    board.clear()
+
+    for i in range(0, LINE_LEN):
+        line = [False] * LINE_LEN
         board.append(line)
 
     return board
@@ -71,15 +83,53 @@ def count_cell(target_cell_v, target_cell_h, count_board):
 
     return count
 
+def galaxy(v, h, board):
+    for i in range(v, v + 2):
+        for j in range(h, h + 6):
+            board[v + i][h + j] = True
+
+    for i in range(v, v + 6):
+        for j in range(h + 7, h + 9):
+            board[v + i][h + j] = True
+
+    for i in range(v + 3, v + 9):
+        for j in range(h, h + 2):
+            board[v + i][h + j] = True
+
+    for i in range(v + 7, v + 9):
+        for j in range(h + 3, h + 9):
+            board[v + i][h + j] = True
+
+    return board
+
+def grider(v, h, board):
+    board[v][h + 1] = True
+    board[v + 1][h + 2] = True
+    board[v + 2][h + 0] = True
+    board[v + 2][h + 1] = True
+    board[v + 2][h + 2] = True
+    
+    return board
+
+
+def blinker(v, h, board):
+    board[v][h] = True
+    board[v + 1][h] = True
+    board[v + 2][h] = True
+
+    return board
+
+
 def main():
-    origin_board = initialize_board([])
+    # origin_board = initialize_board([])
+    origin_board = all_false_board([])
+
+    origin_board = grider(0, 0, origin_board)
+
+    origin_board = galaxy(5, 5, origin_board)
+    
 
     display_board(origin_board)
-
-    # blinker
-    origin_board[0][1] = True
-    origin_board[1][1] = True
-    origin_board[2][1] = True
 
 
     while True:
@@ -87,7 +137,7 @@ def main():
 
         origin_board = calc_board(origin_board)
 
-        time.sleep(0.5)
+        time.sleep(SLEEP_TIME)
         clear()
 
 
